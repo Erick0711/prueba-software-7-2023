47d855b84eece6c2637f9c7f0e3e23c4d24e5450
Maicol Erick Arteaga Guzman <88513626+Erick0711@users.noreply.github.com>
2023-08-10 21:10:45 -0400

    add all entties

---
+++ backend/entidades/Common.cs
@@ -0,0 +1,8 @@
+namespace backend.entidades
+{
+    public class Common{
+        public string NombreProducto {get; set;}
+        public DateTime FechaRegistro {get; set;}
+        public int EstadoRegistro {get; set;}
+    }
+}
